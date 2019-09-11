868f717f068402fccb8c7c844ba34a284bfb421f
Joe Lekie <joe.lekie@gmail.com>
2019-09-11 09:20:18 -0500

    Develop Sync

---
+++ src/defs/handlebars-helpers.d.ts
@@ -0,0 +1,7 @@
+declare module 'handlebars-helpers' {
+    function handlebarsHelpers(options?: any): any;
+    function handlebarsHelpers(collections?: string[]): any;
+
+    namespace handlebarsHelpers {}
+    export = handlebarsHelpers;
+}
